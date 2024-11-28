ff7ff6aec7edb4cd4b83f9835abffa314f630798
DanielWar01 <danielalejoguerra@gmail.com>
2024-11-28 17:54:27 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/springbootjpaexam/SpringbootJpaExamApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.springbootjpaexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uptc/springbootjpaexam/SpringbootJpaExamApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.springbootjpaexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
